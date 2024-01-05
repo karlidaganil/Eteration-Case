0e65625ec0dbcaa34003a53453820dd55109ad38
Anıl Karlıdağ <anil.karlidag@globit.com.tr>
2024-01-05 19:53:29 +0300

    Add height and padding to main container, adjust box title position, and implement sorting functionality

--- src/pages/Home/components/Filters/components/SortBy/SortBy.jsx
+++ src/pages/Home/components/Filters/components/SortBy/SortBy.jsx
@@ -1,7 +1,23 @@
+import { useState } from "react";
+import { Radio, Space } from "antd";
 import Box from "../Box/Box";
 
 const SortBy = () => {
-  return <Box title="Sort By">FORM</Box>;
+  const [value, setValue] = useState(1);
+  const onChange = (e) => setValue(e.target.value);
+
+  return (
+    <Box title="Sort By">
+      <Radio.Group onChange={onChange} value={value}>
+        <Space direction="vertical">
+          <Radio value={1}>Old to new</Radio>
+          <Radio value={2}>New to old</Radio>
+          <Radio value={3}>Price hight to low</Radio>
+          <Radio value={3}>Price low to High </Radio>
+        </Space>
+      </Radio.Group>
+    </Box>
+  );
 };
 
 export default SortBy;
