0624421ccc4687ef6dde7628dbe2fded34fa56c7
Anıl Karlıdağ <anil.karlidag@globit.com.tr>
2024-01-05 20:20:45 +0300

    Add scrollbar styles and update box height

--- src/pages/Home/components/Filters/components/Brands/Brands.jsx
+++ src/pages/Home/components/Filters/components/Brands/Brands.jsx
@@ -1,7 +1,51 @@
+import { Checkbox, Input } from "antd";
+import { SearchOutlined } from "@ant-design/icons";
+
 import Box from "../Box/Box";
 
+import "./style.scss";
+
+const options = [
+  {
+    label: "Apple",
+    value: "Apple",
+  },
+  {
+    label: "Pear",
+    value: "Pear",
+  },
+  {
+    label: "Orange",
+    value: "Orange",
+  },
+  {
+    label: "Apple",
+    value: "Apple1",
+  },
+  {
+    label: "Pear",
+    value: "Pear2",
+  },
+  {
+    label: "Orange",
+    value: "Orange3",
+  },
+];
 const Brands = () => {
-  return <Box title="Brands">FORM</Box>;
+  const onChange = (checkedValues) => {
+    console.log("checked = ", checkedValues);
+  };
+  return (
+    <Box title="Brands">
+      <Input placeholder="Search" prefix={<SearchOutlined />} />
+      <Checkbox.Group
+        className="checkbox-vertical"
+        options={options}
+        defaultValue={["Pear"]}
+        onChange={onChange}
+      />
+    </Box>
+  );
 };
 
 export default Brands;
