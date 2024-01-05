0b05983cfbbad446f28e37db6346f20000a09998
Anıl Karlıdağ <anil.karlidag@globit.com.tr>
2024-01-05 23:38:43 +0300

    Update Header component with search input and styling

--- src/layout/Header/Header.jsx
+++ src/layout/Header/Header.jsx
@@ -1,17 +1,19 @@
 import "./style.scss";
 import Portfeil from "../../assets/icons/Portfeil.png";
 import Profile from "../../assets/icons/Profile.png";
+import { Input } from "antd";
+import { SearchOutlined } from "@ant-design/icons";
 
 const Header = () => {
   return (
     <header>
       <h3>Eteration</h3>
-      <input type="text" placeholder="Search" />
+      <Input placeholder="Search" prefix={<SearchOutlined />} />
       <div>
-        <img src={Portfeil} alt="profile" />
-        <span>117.000</span>
+        <img src={Portfeil} alt="prort" />
+        <span> 117.000$ </span>
         <img src={Profile} alt="profile" />
-        <span>Anıl</span>
+        <span> Anıl</span>
       </div>
     </header>
   );
