94361220c254707ffd2397022d3c914ca247cbe1
Anıl Karlıdağ <anil.karlidag@globit.com.tr>
2024-01-05 20:24:12 +0300

    Add model selection functionality and search feature

--- src/pages/Home/components/Filters/components/Model/Model.jsx
+++ src/pages/Home/components/Filters/components/Model/Model.jsx
@@ -1,7 +1,39 @@
+import { Checkbox, Input } from "antd";
+import { SearchOutlined } from "@ant-design/icons";
+
 import Box from "../Box/Box";
 
-const Models = () => {
-  return <Box title="Models">FORM</Box>;
+import "./style.scss";
+
+const options = [
+  {
+    label: "11",
+    value: "Apple",
+  },
+  {
+    label: "12 Pro",
+    value: "Pear",
+  },
+  {
+    label: "13 Pro Max",
+    value: "Orange",
+  },
+];
+const Model = () => {
+  const onChange = (checkedValues) => {
+    console.log("checked = ", checkedValues);
+  };
+  return (
+    <Box title="Model">
+      <Input placeholder="Search" prefix={<SearchOutlined />} />
+      <Checkbox.Group
+        className="checkbox-vertical"
+        options={options}
+        defaultValue={["Pear"]}
+        onChange={onChange}
+      />
+    </Box>
+  );
 };
 
-export default Models;
+export default Model;
